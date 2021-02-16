567f7f2b02cfc0aaa3f57e08027f29e7134c53d0
psychopratek <adamballard86@gmail.com>
2021-02-16 11:56:33 +0000

    Update Valheim_Backup.ps1

--- Valheim_Backup.ps1
+++ Valheim_Backup.ps1
@@ -1,8 +1,8 @@
-﻿$car_name = "Vandalore"
+﻿$char_name = "Vandalore"
 $wor_name = "Ragnaroc"
 
-$char_loc = $home+"\AppData\LocalLow\IronGate\Valheim\characters\vandalore.fch"
-$wor_loc = $home+"\AppData\LocalLow\IronGate\Valheim\worlds\ragna*"
+$char_loc = $home+"\AppData\LocalLow\IronGate\Valheim\characters\"+"$char_name"+".fch"
+$wor_loc = $home+"\AppData\LocalLow\IronGate\Valheim\worlds\"+"$wor_name"+"*"
 $backup_loc = $home+"\Documents\Valheim_Backup\"
 $date = Get-Date -format "yyyy_MM_dd_hh_mm_ss"
 
