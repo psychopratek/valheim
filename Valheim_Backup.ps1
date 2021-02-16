812526c05e76f7dfef94c61f783d30609f641198
psychopratek <adamballard86@gmail.com>
2021-02-16 12:39:30 +0000

    Update Valheim_Backup.ps1

--- Valheim_Backup.ps1
+++ Valheim_Backup.ps1
@@ -8,5 +8,16 @@ $date = Get-Date -format "yyyy_MM_dd_hh_mm_ss"
 
 $new_fol = New-Item -Path $backup_loc -Name $date -ItemType "directory"
 
+try {
 Copy-Item $char_loc -Destination $new_fol
+}
+catch {
+	Write-Error "Not copied!"
+}
+
+try {
 Copy-Item $wor_loc -Destination $new_fol
+}
+catch {
+	Write-Error "Not compied!"
+}
